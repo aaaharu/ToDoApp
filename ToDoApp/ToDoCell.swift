3392396dd1c5f9201a9bde85c3a026d542acc785
Kim Eunji <qoopej@gmail.com>
2023-05-30 20:15:33 +0900

    commit

---
+++ ToDoApp/ToDoCell.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoCell.swift
+//  ToDoApp
+//
+//  Created by 김은지 on 2023/05/30.
+//
+import UIKit
+import Foundation
+
+class ToDoCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    
+    
+    
+}
