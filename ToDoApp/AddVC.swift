117ee14d53f9569e2aaf3bd6d14b514382e91869
Kim Eunji <qoopej@gmail.com>
2023-05-31 21:17:02 +0900

    POST, Search commit

--- ToDoApp/AddVC.swift
+++ ToDoApp/AddVC.swift
@@ -10,13 +10,29 @@ import UIKit
 
 class AddVC: UIViewController {
     
+    @IBOutlet weak var backBtn: UIBarButtonItem!
+    @IBOutlet weak var toDoTF: UITextField!
     @IBOutlet weak var finishBtn: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI()
+        
+        
+        
     }
-
+    
+    @IBAction func backBtnClicked(_ sender: UIBarButtonItem) {
+        
+      
+        
+    }
+    
+    
+    
     fileprivate func setupUI() {
         
         finishBtn.clipsToBounds = true
@@ -24,7 +40,77 @@ class AddVC: UIViewController {
         
     }
     
-
-    @IBAction func backBtnClicked(_ sender: UIBarButtonItem) {
+    @IBAction func finishBtnCLicked(_ sender: UIButton) {
+        
+        guard let text = toDoTF.text, !text.isEmpty else { return }
+        
+        guard text.count > 0 else { return }
+        
+        callPost(text)
+        
+        //노티 등록
+        
+        NotificationCenter.default.post(name: Notification.Name("CustomNotification"), object: nil)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            self.navigationController?.popViewController(animated: true)
+           }
+           
+        
+    }
+    
+    
+    
+    fileprivate func callPost(_ AddToDo: String){
+        print(#fileID, #function, #line, "-  주석 ")
+        
+        let urlString: String = "https://phplaravel-574671-2962113.cloudwaysapps.com/api/v1/todos"
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        
+        
+        // JSON 데이터
+        let priJsonData: [String: Any] = [
+            "title" : "\(AddToDo)",
+            "is_done" : false
+        ]
+        
+        // JSON 데이터로 직렬화하는 기능 - JSONSerialization
+        let jsonData = try? JSONSerialization.data(withJSONObject: priJsonData)
+        
+        // HTTP 요청
+        var urlReuqest = URLRequest(url: url)
+        urlReuqest.httpMethod = "POST"
+        urlReuqest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlReuqest.httpBody = jsonData
+        
+        
+        // 요청 보내기 - URLSession
+        let task = URLSession.shared.dataTask(with: urlReuqest) { data, response, error in
+            
+            if let error = error {
+                print(#fileID, #function, #line, "- 할일 목록 추가 실패 \(error.localizedDescription)")
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  httpResponse.statusCode == 200 else {     print(#fileID, #function, #line, "- 할일 목록 추가 실패(응답)")
+                return
+            }
+            
+            print(#fileID, #function, #line, "- 할 일 목록 추가 성공(응답)")
+        
+            
+            
+        }
+        
+        task.resume()
+        
+        
+        
+        
     }
 }
+
+
--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class ViewController: UIViewController{
     
+    // 검색 작업 아이템
+    var searchDispatchWorkItem : DispatchWorkItem? = nil
     
     @IBOutlet weak var plusBtn: UIButton!
- 
     
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
     var toDoList: [Post] = []
@@ -27,6 +30,49 @@ class ViewController: UIViewController{
         setupUI()
         toDoCall()
         
+        // 노티 - 수신기 등록
+        NotificationCenter.default.addObserver(self, selector: #selector(toDoRecall), name: Notification.Name("CustomNotification"), object: nil)
+        
+        // 검색
+        searchBar.searchTextField.addTarget(self, action: #selector(searchBarInput(_:)), for: .editingChanged)
+    }
+    
+    // 한글 String을 URL로 변환(한글로 query 검색 시 에러 뜸)
+    func encodeKoreanToURL(_ KoreanString: String) -> String {
+        
+    
+        let encodedString = KoreanString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        
+        return encodedString
+    }
+    
+    @objc fileprivate func searchBarInput(_ searchTextField: UITextField) {
+            print(#fileID, #function, #line, "- <# 주석 #>")
+        
+        let urlQuery: String? = encodeKoreanToURL(searchTextField.text ?? "")
+        
+        if let query = urlQuery {
+            
+            // 검색어가 입력되면 기존 작업 취소
+            searchDispatchWorkItem?.cancel()
+            
+            let dispatchWorkItem = DispatchWorkItem(block: {
+                // 백그라운드 - 사용자 입력 userInteractive
+                DispatchQueue.global(qos: .userInteractive).async {
+                    print(#fileID, #function, #line, "- 검색 API 호출하기 userInput: (searchTextField)")
+                    
+                    self.callSearchGET(query)
+                }
+            })
+            
+            // 기존작업을 나중에 취소하기 위해 메모리 주소 일치 시켜줌
+            self.searchDispatchWorkItem = dispatchWorkItem
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.7, execute: dispatchWorkItem)
+            
+            
+            //        searchPhoto(searchInput)
+        }
         
     }
     
@@ -34,7 +80,108 @@ class ViewController: UIViewController{
     fileprivate func setupUI() {
         plusBtn.clipsToBounds = true
         plusBtn.layer.cornerRadius = 0.5 * plusBtn.bounds.size.width
+        
     }
+    
+    @objc fileprivate func toDoRecall() {
+        print(#fileID, #function, #line, "-  주석 ")
+        
+        let urlString: String = "https://phplaravel-574671-2962113.cloudwaysapps.com/api/v1/todos?page=1&order_by=desc&per_page=10"
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        let urlReuqest = URLRequest(url: url)
+        URLSession.shared.dataTask(with: urlReuqest) { data, response, error in
+            
+            
+            guard let data = data else { return }
+            //
+            //            if let jsonString = String(data: data, encoding: .utf8) {
+            //                    print(#fileID, #function, #line, "- \(jsonString)")
+            //            }
+            do {
+                
+                let todoResponse: ToDoResponse = try JSONDecoder().decode(ToDoResponse.self, from: data)
+                print(#fileID, #function, #line, "포스트\(todoResponse) ")
+                
+                
+                self.toDoList = todoResponse.data
+                
+                
+                
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
+                
+            } catch {
+                print(#fileID, #function, #line, "- \(error)")
+            }
+            
+            
+            
+            
+            
+            
+        }.resume()
+        
+        
+    }
+    
+    @objc fileprivate func callSearchGET(_ query: String) {
+        print(#fileID, #function, #line, "-  주석 ")
+        
+        self.toDoList.removeAll()
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+        
+        let urlString: String = "https://phplaravel-574671-2962113.cloudwaysapps.com/api/v1/todos/search?query=\(query)&order_by=desc&page=1&per_page=10"
+        
+        guard let url = URL(string: urlString) else { return     print(#fileID, #function, #line, "- Url 오류! ")}
+        
+            print(#fileID, #function, #line, "- \(url)")
+        
+        let urlReuqest = URLRequest(url: url)
+        URLSession.shared.dataTask(with: urlReuqest) { data, response, error in
+                print(#fileID, #function, #line, "- <# 주석 #>")
+            
+            guard let data = data else { return }
+            //
+            //            if let jsonString = String(data: data, encoding: .utf8) {
+            //                    print(#fileID, #function, #line, "- \(jsonString)")
+            //            }
+            do {
+                
+                let todoResponse: ToDoResponse = try JSONDecoder().decode(ToDoResponse.self, from: data)
+                print(#fileID, #function, #line, "포스트\(todoResponse) ")
+                
+            
+                self.toDoList = todoResponse.data
+                
+                print(#fileID, #function, #line, "- \(self.toDoList.count)")
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
+                
+            } catch {
+                print(#fileID, #function, #line, "- \(error)")
+            }
+            
+            
+            
+            
+            
+            
+        }.resume()
+        
+        
+    }
+    
     
     fileprivate func toDoCall() {
         print(#fileID, #function, #line, "-  주석 ")
@@ -46,14 +193,14 @@ class ViewController: UIViewController{
         let urlReuqest = URLRequest(url: url)
         URLSession.shared.dataTask(with: urlReuqest) { data, response, error in
             
-         
+            
             guard let data = data else { return }
-//
-//            if let jsonString = String(data: data, encoding: .utf8) {
-//                    print(#fileID, #function, #line, "- \(jsonString)")
-//            }
+            //
+            //            if let jsonString = String(data: data, encoding: .utf8) {
+            //                    print(#fileID, #function, #line, "- \(jsonString)")
+            //            }
             do {
-               
+                
                 let todoResponse: ToDoResponse = try JSONDecoder().decode(ToDoResponse.self, from: data)
                 print(#fileID, #function, #line, "포스트\(todoResponse) ")
                 
@@ -97,14 +244,14 @@ extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath) as? ToDoCell {
-                print(#fileID, #function, #line, "- <# 주석 #>")
+            print(#fileID, #function, #line, "- <# 주석 #>")
             
-            
-            if cell.label != nil {
-                    print(#fileID, #function, #line, "- 레이블이 연결되었습니다.")
-            } else {
-                    print(#fileID, #function, #line, "- 레이블이 연결되지 않았습니다!")
-            }
+//
+//            if cell.label != nil {
+//                print(#fileID, #function, #line, "- 레이블이 연결되었습니다.")
+//            } else {
+//                print(#fileID, #function, #line, "- 레이블이 연결되지 않았습니다!")
+//            }
             
             let cellData: Post = toDoList[indexPath.row]
             
@@ -113,14 +260,14 @@ extension ViewController: UITableViewDataSource {
             } else {
                 cell.label?.text = "제목 없음"
             }
-           
+            
             return cell
         }
-            
-            return UITableViewCell()
-            
-            
-            
+        
+        return UITableViewCell()
+        
+        
+        
     }
     
     
